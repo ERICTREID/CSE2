d1a275fa53ed9db749e40e7018bc13e3cbee483b
erictreid1 <eric@reidclan.us>
2015-08-28 20:01:09 +0000

    final WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,18 @@
+//Eric Reid
+//Homework 1
+
+public class WelcomeClass{
+    
+    public static void main(String[] args){
+        //write the message
+        System.out.println("  -----------");
+        System.out.println("  | WELCOME |");
+        System.out.println("  -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\  ");
+        System.out.println("<-E--T--R--2--1--8->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("I am a member of the Marching 97. So PSYCHE!!!!");
+    }
+}
