0614b3a63c623e9fc13703874e380f8f5f3b16ee
erictreid1 <eric@reidclan.us>
2015-09-18 15:52:47 +0000

    updated CardGenerator

--- lab04/CardGenerator.java
+++ lab04/CardGenerator.java
@@ -6,7 +6,6 @@
 public class CardGenerator{
     public static void main(String[] args){
         int card=(int) (Math.random()*53)+1;//create a random integer between 1 and 52
-        System.out.println(card);
         //diamonds category
         if(card<=13){
             //test to see if in diamond range
@@ -46,53 +45,52 @@ public class CardGenerator{
                     System.out.println("You picked the ace of clubs");//print ace statement
                 break;
                 default:
-                    System.out.println("You picked the "+card+" of clubs");//print all other statments
+                    System.out.println("You picked the "+(card-13)+" of clubs");//print all other statments
                 break;
             }
         }
-        if(card<=38&&card>26){
+        if(card<=39&&card>26){
             //test to see if in hearts range
             switch (card){
                 //test to see if higher than 10 or equal to one and change output accordingly
-                case 36:
+                case 37:
                     System.out.println("You picked the jack of hearts");//print jack  statement
                 break;
-                case 37:
+                case 38:
                     System.out.println("You picked the queen of hearts");//print queen statement
                 break;
-                case 38:
+                case 39:
                     System.out.println("You picked the king of hearts");//print king statement
                 break;
                 case 27:
                     System.out.println("You picked the ace of hearts");//print ace statement
                 break;
                 default:
-                    System.out.println("You picked the "+card+" of hearts");//print all other statments
+                    System.out.println("You picked the "+(card-26)+" of hearts");//print all other statments
                 break;
             }
         }
-        if(card>38){
+        if(card>39){
             //test to see if in spades range
             switch (card){
                 //test to see if higher than 10 or equal to one and change output accordingly
-                case 50:
+                case 51:
                     System.out.println("You picked the jack of spades");//print jack  statement
                 break;
-                case 51:
+                case 52:
                     System.out.println("You picked the queen of spades");//print queen statement
                 break;
-                case 52:
+                case 53:
                     System.out.println("You picked the king of spades");//print king statement
                 break;
                 case 39:
                     System.out.println("You picked the ace of spades");//print ace statement
                 break;
                 default:
-                    System.out.println("You picked the "+card+" of spades");//print all other statments
+                    System.out.println("You picked the "+(card-39)+" of spades");//print all other statments
                 break;
             }
         }
                     
         }
-        
     }
